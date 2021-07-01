3f4258afc55f288948b99c4270de7aa62f27be82
Iliya Lunev <lunevig90@gmail.com>
2021-07-01 15:45:33 +0300

    task 5 done

---
+++ src/task-05.js
@@ -0,0 +1,10 @@
+const inputEl = document.querySelector("#name-input");
+const outputEl = document.querySelector("span#name-output");
+
+function onInput() {
+    if (inputEl.value !== "") outputEl.textContent = inputEl.value
+    else outputEl.textContent = "незнакомец";
+}
+
+inputEl.addEventListener(`input`, onInput);
+
