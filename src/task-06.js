30f89962151adcede218200ecb0b6aadffd8fd5d
Iliya Lunev <lunevig90@gmail.com>
2021-07-01 16:32:27 +0300

    task 6 done

---
+++ src/task-06.js
@@ -0,0 +1,22 @@
+const getInput = document.querySelector(`#validation-input`);
+
+const getDataLength = Number(getInput.getAttribute(`data-length`));
+
+
+const inputIsValid = getInput.addEventListener(`focus`, onFormInput);
+
+function onFormInput(event) {
+    
+   
+
+
+    if (event.currentTarget.value.length < getDataLength) {
+        getInput.classList.add('invalid');
+        getInput.classList.remove('valid');
+  } else {
+        getInput.classList.remove(`invalid`);
+        getInput.classList.add('valid');
+    };
+};
+
+console.log(getInput);
